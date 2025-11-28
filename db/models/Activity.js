fe6c6dc5c3fc48a9c0f6bde9c1e59afd5e8aaec4
Jakob-M-101 <20402329+jakobm101@users.noreply.github.com>
2025-11-28 13:58:26 +0100

    env

--- db/models/Activity.js
+++ db/models/Activity.js
@@ -4,8 +4,14 @@ const { Schema } = mongoose;
 
 const activitySchema = new Schema({
   title: { type: String, required: true },
+  categories: [{ type: Schema.Types.ObjectId, ref: "Category" }], // POPULATE requires this (see API)
+  area: { type: String },
+  country: { type: String },
+  description: { type: String },
+  imageUrl: { type: String },
 });
 
-const Activity= mongoose.models.Activity|| mongoose.model("Activity", activitySchema);
+const Activity =
+  mongoose.models.Activity || mongoose.model("Activity", activitySchema);
 
 export default Activity;
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,11 +1,12 @@
 import dbConnect from "@/db/connect";
 import Activity from "@/db/models/Activity";
+import Category from "@/db/models/Category"; //populate needs this
 
 export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const activities = await Activity.find();
+    const activities = await Activity.find().populate("categories"); // this line populates the categories
 
     response.status(200).json(activities);
     return;
